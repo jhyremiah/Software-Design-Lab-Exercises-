d66069d045e38c7e314cc1059827f75f90efeba0
jhyremiah <106580188+jhyremiah@users.noreply.github.com>
2022-06-02 08:14:43 +0800

    Add files via upload

---
+++ sparsearray.py
@@ -0,0 +1,16 @@
+class SparseArray:
+	def __init__(self, A):
+		self._orig = A
+		self._storage = {}
+		for idx, elem in enumerate(A):
+			if elem != None:
+				self._storage[idx] = elem
+
+	def __len__(self):
+		return len(self._storage)
+
+	def __getitem__(j):
+		return self._storage[j]
+
+	def __setitem__(j, e):
+		self._storage[j] = e
